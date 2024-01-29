76b1693028275040287f4e9f059a3ced6fec5c5f
Sagar <Sagarchawan4646@gmail.com>
2024-01-29 22:51:36 +0530

    Two Sums javascript

---
+++ NeetCode/Arrays & Hashing/Two Sums/TwoSums.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+  const checkingHashValue = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const valueToCheck = target - nums[i];
+    if (checkingHashValue.has(valueToCheck)) {
+      return [checkingHashValue.get(valueToCheck), i];
+    }
+
+    checkingHashValue.set(nums[i], i);
+    target;
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
