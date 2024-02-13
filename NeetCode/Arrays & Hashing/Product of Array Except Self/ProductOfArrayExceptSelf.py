da953f5abb89ac94b53b20c5cb07b85e64e5c51c
Sagar <Sagarchawan4646@gmail.com>
2024-02-13 23:16:30 +0530

    Product of Array except itself

---
+++ NeetCode/Arrays & Hashing/Product of Array Except Self/ProductOfArrayExceptSelf.py
@@ -0,0 +1,21 @@
+# Time O(n)
+# Space O(n)
+
+# https://leetcode.com/problems/product-of-array-except-self/description/
+
+class Solution:
+    def productExceptSelf(self, nums):
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) -1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+result = Solution()
+print(result.productExceptSelf([1,2,3,4]))
